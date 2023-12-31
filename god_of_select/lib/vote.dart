3943d1acde8811ae2577ebc669c4c2baa3fd0aea
RDDcat <yukmaro@gmail.com>
2023-12-31 21:33:06 +0900

    [UPDATE] 뒤로가기 기능 추가

--- god_of_select/lib/vote.dart
+++ god_of_select/lib/vote.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -24,7 +26,7 @@ class VoteState extends State<Vote>{
   Future<void> sendVote(String contentId) async {
     print('투표함');
     final response = await http.post(
-      Uri.parse('${BASE_URL}issue/vote'), // 실제 엔드포인트로 변경
+      Uri.parse('${BASE_URL}issue/vote'),
       body: jsonEncode({
         'user_id': uuid,
         'issue_id': choose_data['id'],
@@ -37,6 +39,25 @@ class VoteState extends State<Vote>{
       // 성공적으로 투표가 이루어진 경우
       // 다음 질문 데이터를 가져와서 셋팅하는 등의 로직을 추가할 수 있습니다.
       print('success');
+
+      // 데이터 변경
+      Map<String, dynamic>? getRandomElement(List<Map<String, dynamic>> dataList) {
+        if (dataList.isEmpty) {
+          return null;
+        }
+
+        Random random = Random();
+        int randomIndex = random.nextInt(dataList.length);
+
+        return dataList[randomIndex];
+      }
+      choose_data = getRandomElement(all_data)!;
+
+      // 페이지 재생성
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Vote()),
+      );
     } else {
       // 투표 요청이 실패한 경우
       print('Failed to send vote');
@@ -51,6 +72,16 @@ class VoteState extends State<Vote>{
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // 뒤로가기 버튼을 눌렀을 때 홈 화면으로 이동
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => FirstPage()), // Home으로 이동하도록 수정
+            );
+          },
+        ),
       ),
       body: Container(
           color: Colors.black,
